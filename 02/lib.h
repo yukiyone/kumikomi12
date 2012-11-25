0b03da826a14c20211cfc684791d73743427ebce
Yuki YONEZAWA <yuki.yonezawa.86@gmail.com>
2012-11-25 13:51:59 +0900

    compilable

--- 02/lib.h
+++ 02/lib.h
@@ -3,5 +3,14 @@
 
 int putc(unsigned char c); /*show 1 char*/
 int puts(unsigned char *str); /*show some chars*/
+int putxval(unsigned long value, int column);
+
+void *memset(void *b, int c, long len);
+void *memcpy(void *dst, const void *src, long len);
+int memcmp(const void *b1, const void *b2, long len);
+int strlen(const char *s);
+char *strcpy(char *dst, const char *src);
+int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, int len);
 
 #endif
